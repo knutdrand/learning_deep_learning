ecdabf4948e0e99c073e4032703affbe79547124
Knut Rand <knutdrand@gmail.com>
2020-09-01 15:49:42 +0200

    gradient descent working on simple linear model

---
+++ learning_deep_learning/linearmodel.py
@@ -0,0 +1,65 @@
+from dataclasses import dataclass
+import numpy as np
+
+
+"""
+x = [1,4
+     2,5
+     3,6]
+y = [10, 20]
+W = [100, 200, 300]
+p = [a, b]
+
+Wx = [w1.x, w2.x]
+
+
+x=(3x1)
+W=(2x3)
+Wx=(2x1)
+
+
+[ux+vy+wz-t]**2
+du=2e*x
+dv=2e*y
+dw=2e*z
+[2e*
+"""
+"""
+d(Wx)/dW = [d([x^TW^T]^T)/dW] = x^TT
+"""
+
+def mse(a, b):
+    return np.mean((a-b)**2)
+
+
+@dataclass
+class SimpleLinearModel:
+    W: np.array
+
+    def predict(self, x):
+        assert x.shape[0] == self.W.shape[-1]
+        return self.W.dot(x)
+
+    def get_gradient(self, x, y):
+        """
+        l = sum[(Wx-y)^2]/n
+        dl/dW = 2(Wx-y)*x^T
+        """
+        predicted = self.predict(x)
+        loss = mse(predicted, y)
+        d_loss_on_W = 2*np.mean((predicted-y)*x, axis=1)
+        return d_loss_on_W[None, :]
+
+    def update_model(self, gradient, rate=0.01):
+        self.W -= gradient*rate
+
+    def train(self, X, y, n=1):
+        for _ in range(n):
+            gradient = self.get_gradient(X, y)
+            self.update_model(gradient, 0.5)
+            print(self.W)
+
+    def generate_data(self, n, epsilon=1):
+        X = np.random.rand(self.W.shape[1], n)
+        y = self.predict(X)+(np.random.rand(1, n)-0.5)*epsilon
+        return X, y
---
+++ tests/test_linearmodel.py
@@ -0,0 +1,55 @@
+import pytest
+import numpy as np
+from learning_deep_learning.linearmodel import SimpleLinearModel
+
+@pytest.fixture
+def W():
+    return np.array([[4., 5., 6.]])
+
+@pytest.fixture
+def true_W():
+    return np.array([[10., 7., 19.]])
+
+
+def test_predict_simple_linear(W):
+    x = np.array([[10, 20, 30]]).T
+    res = SimpleLinearModel(W).predict(x)
+    wanted = np.array([[40+100+180]]).T
+    assert np.array_equal(res, wanted)
+
+def test_multi_gradient(W):
+    x = np.array([[10, 20, 30],
+                  [3, 2, 1]]).T
+    
+    y = np.array([310, 20])
+    delta = SimpleLinearModel(W.copy()).get_gradient(x, y)
+    print(np.array([[10, 8]])*x)
+    true = 2*(np.sum(np.array([[10, 8]])*x, axis=1)/2)[None, :]
+    assert np.array_equal(delta, true)
+
+def test_one_gradient(W):
+    x = np.array([[10, 20, 30]]).T
+    y = 310
+    delta = SimpleLinearModel(W.copy()).get_gradient(x, y)
+    assert np.array_equal(delta, 2*x.T*10)
+
+
+def test_update(W):
+    model = SimpleLinearModel(W.copy())
+    model.update_model(np.array([[100., 200., 300.]]))
+    assert np.array_equal(model.W, W-np.array([[1, 2, 3]]))
+
+def test_train(W, true_W):
+    X, y = SimpleLinearModel(true_W).generate_data(10000, 0.001)
+    model = SimpleLinearModel(W)
+    model.train(X, y, 1000)
+    assert np.allclose(model.W, true_W)
+
+def _test_predict_linear():
+    W = np.array([[1, 2, 3], [4, 5, 6]])
+    x = np.array([[10, 20, 30]]).T
+    res = LinearModel(W).predict(x)
+    wanted = np.array([[10+40+90, 
+                        40+100+180]]).T
+    assert np.array_equal(res, wanted)
+        
